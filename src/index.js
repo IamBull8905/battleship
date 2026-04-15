a7d7f1f07f4b710168b7c6b8675e3c34fc288f78
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-04-15 20:38:39 +0100

    Initialise Ship objects using a class and add relevant methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    if (this.hits === this.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export { Ship };
---
+++ src/ships.test.js
@@ -0,0 +1,23 @@
+import { Ship } from "./index.js";
+
+describe("Ship initialisation & method testing", () => {
+  test("Ship initialises correctly", () => {
+    const newShip = new Ship(3);
+    expect(newShip.length).toBe(3);
+    expect(newShip.hits).toBe(0);
+    expect(newShip.sunk).toBeFalsy();
+  });
+
+  test("Ship can get hit successfully", () => {
+    const newShip = new Ship(3);
+    newShip.hit();
+    expect(newShip.hits).toBe(1);
+  });
+
+  test("Ship can accurately report if it has been sunk", () => {
+    const newShip = new Ship(2);
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.isSunk()).toBeTruthy();
+  });
+});
